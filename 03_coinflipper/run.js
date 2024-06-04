ea12a8aaa657805beb28cafab800265f06adb740
Henry-H08 <143623813+Henry-H08@users.noreply.github.com>
2024-06-04 18:42:44 +0000

    javascript

---
+++ 03_coinflipper/run.js
@@ -0,0 +1,30 @@
+const Icon = document.getElementById('coin');
+const tossBtn = document.getElementById('toss-button');
+const result = document.querySelector('.result');
+Icon.insertAdjacentElement('afterend', result);
+tossBtn.addEventListener('click', () => {
+    tossBtn.disabled = true;
+    tossCoinFunction();
+});
+
+function tossCoinFunction() {
+    const randomVal = Math.random();
+    const faceCoin = randomVal < 0.5 ? 'heads' : 'tails';
+    const imageUrl = faceCoin === 'heads' ?
+''
+''
+
+    Icon.classList.add('flip');
+    setTimeout(() => {
+        Icon.innerHTML =
+            `<img src="${imageUrl}" alt="${faceCoin}">`;
+        Icon.classList.remove('flip');
+        setTimeout(() => {
+            result.textContent = `Result: ${faceCoin}`;
+            result.computedStyleMap.opacity = 1;
+            tossBtn.disabled = false;
+
+        }, 500);
+
+    }, 1000);
+}
